e31fd5b596f2777ee1975d9a2e8f2593b43339a3
Fernandocuamba <fernandocuamba28@gmail.com>
2021-12-13 05:00:32 -0800

    actualizando as configuracoes dos cupons

--- src/routes/cupao.js
+++ src/routes/cupao.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const CupaoController = require("../controllers/cupao");
 const { authenticateToken, isAdmin } = require("../config/authValidation");
 
-router.post("/",authenticateToken,CupaoController.Insert);
+router.post("/",CupaoController.Insert);
 router.get("/",authenticateToken,CupaoController.SearchAll);
 router.put("/validar/:codigo", CupaoController.validate);
-router.put("/:id",authenticateToken, CupaoController.update);
-
+router.put("/:id",CupaoController.update);
 
 module.exports = router;
